a31cef407a4c815c607257fb2a519c303632d05a
4him <rudoi.edward@gmail.com>
2025-03-03 23:32:08 +0200

    removed unused comments

--- gateway-service/src/main/java/com/example/gatewayservice/config/WebSecurityConfiguration.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/WebSecurityConfiguration.java
@@ -3,15 +3,11 @@ package com.example.gatewayservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-// Используем EnableWebFluxSecurity для реактивного стека
 @EnableWebFluxSecurity
 public class WebSecurityConfiguration {
 
